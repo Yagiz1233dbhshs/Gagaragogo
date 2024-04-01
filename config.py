4c1180ca70d19779d6d95d302cc9af09d2dbbace
Yagiz1233dbhshs <163686002+Yagiz1233dbhshs@users.noreply.github.com>
2024-04-02 01:01:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/EfsunkarBots")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Efsunkarr")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", False)
 AUTO_SUGGESTION_TIME = int(
     getenv("AUTO_SUGGESTION_TIME", "500"))
